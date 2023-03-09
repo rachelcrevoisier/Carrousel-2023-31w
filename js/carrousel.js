f62b826340fb93e532e3bca48a512c0c1ce0b116
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-03-09 15:22:55 -0500

    c12 mise en place boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,17 @@
 (function(){
 // fonction IFEE
 console.log("début du carrousel");
+let bouton__ouvrir = document.querySelector('.bouton__ouvrir')
+let elmCarrousel = document.querySelector('.carrousel')
+let elmBouton__x = document.querySelector('.bouton__x')
+console.log(bouton__ouvrir.tagName);
+bouton__ouvrir.addEventListener('mousedown', function(){
+    console.log('boite modale');
+    elmCarrousel.classList.add('carrousel--ouvrir')
+
+})
+    elmBouton__x.addEventListener('mousedown', function () {
+        console.log('boite modale');
+        elmCarrousel.classList.remmove('carrousel--ouvrir')
+})
 })()
